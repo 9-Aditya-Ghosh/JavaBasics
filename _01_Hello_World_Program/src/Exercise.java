3e1924640bd0183ba0e1a9c862ad85b7d2625b07
Aditya Ghosh <ghoshadityaxd@gmail.com>
2024-05-05 19:59:50 +0530

    added exercise for hello world.

--- _01_Hello_World_Program/src/Exercise.java
+++ _01_Hello_World_Program/src/Exercise.java
@@ -1,23 +1,21 @@
 package _01_Hello_World_Program.src;
 
-
 public class Exercise {
- 
+
     public static void main(String[] args) {
 
         /*
-
-        TODO: Print your name , age , in new lines
-
-        Expected output
-
-        Jon Snow
-        25
-
+         * 
+         * TODO: Print your name , age , in new lines
+         * 
+         * Expected output
+         * 
+         * Jon Snow
+         * 25
+         * 
          */
 
-
-        System.out.println("Hello World!");
+        System.out.println("Aditya Ghosh \n21");
     }
- 
+
 }
