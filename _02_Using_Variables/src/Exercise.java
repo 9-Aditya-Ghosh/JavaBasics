ccd1c19fb83c7d3a143b0975f8b097cddbd227a2
Aditya Ghosh <ghoshadityaxd@gmail.com>
2024-05-05 20:24:30 +0530

    Added code for Exercise 2

--- _02_Using_Variables/src/Exercise.java
+++ _02_Using_Variables/src/Exercise.java
@@ -1,24 +1,27 @@
 package _02_Using_Variables.src;
 
-
 public class Exercise {
- 
-   
 
-        /*
+        public static void main(String[] args) {
 
+                /*
+                 * 
+                 * 
+                 * Declare two variables and do the
+                 * following operations and print the result with
+                 * proper output message.
+                 * Add,Subtract,Multiply and Divide
+                 * 
+                 * 
+                 */
 
-        Declare two variables and do the following operations and print the result with proper output message.
-        Add,Subtract,Multiply and Divide
+                double number1 = 345.56;
+                double number2 = 567.89;
 
+                System.out.println("Addition of two numbers : " + (number1 + number2));
+                System.out.println("Subtraction of two numbers : " + (number1 - number2));
+                System.out.println("Multiplication of two numbers : " + (number1 * number2));
+                System.out.println("Division of two numbers : " + (number1 / number2));
+        }
 
-         */
-
-
-
-
-
-
-    
- 
 }
