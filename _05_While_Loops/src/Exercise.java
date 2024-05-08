eb647a5bf35c877c320003ef8e321f72bdf4f916
Aditya Ghosh <ghoshadityaxd@gmail.com>
2024-05-08 23:35:57 +0530

    while loop code added

--- _05_While_Loops/src/Exercise.java
+++ _05_While_Loops/src/Exercise.java
@@ -1,7 +1,21 @@
 package _05_While_Loops.src;
 
-
-
 public class Exercise {
-    //TODO print all  Fibonacci number from 1 to 1000
+    // TODO print all Fibonacci number from 1 to 1000
+    public static void main(String[] args) {
+        int first = 0, second = 1, next;
+        next = first + second;
+        System.out.print(first + " ");
+        System.out.print(second + " ");
+
+        while (next <= 1000) {
+
+            System.out.print(" " + next);
+            first = second;
+            second = next;
+            next = first + second;
+
+        }
+
+    }
 }
