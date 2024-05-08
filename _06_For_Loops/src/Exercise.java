9f7d8fc606a8fb2a083f5a563b4ed7b2f81ba877
Aditya Ghosh <ghoshadityaxd@gmail.com>
2024-05-09 00:21:17 +0530

    for loop code added

--- _06_For_Loops/src/Exercise.java
+++ _06_For_Loops/src/Exercise.java
@@ -1,30 +1,49 @@
 package _06_For_Loops.src;
 
-
-
 public class Exercise {
-    
-         
-       /*P
+        public static void main(String[] args) {
 
-       Print the following patterns
+                /*
+                 * P
+                 * 
+                 * Print the following patterns
+                 * 
+                 * 1.
+                 *
+                 * *
+                 * * *
+                 * * * *
+                 * * * * *
+                 * 
+                 * 
+                 * 2.
+                 * 
+                 * 1
+                 * 8 27
+                 * 64 125 216
+                 * 343 512 728 1000
+                 * 
+                 * 
+                 * 
+                 */
 
-1.
-*
-* *
-* * *
-* * * *
-* * * * *
+                // first patten :
+                for (int i = 1; i <= 5; i++) {
+                        for (int j = 1; j <= i; j++) {
+                                System.err.print("*");
+                        }
+                        System.err.println();
+                }
 
-2.
+                System.err.println("---------------------------------");
 
-1
-8 27
-64 125 216
-349 512 728 1000
-
-
-
-        */
-    
+                // second pattern :
+                int num = 1;
+                for (int i = 1; i <= 4; i++) {
+                        for (int j = 1; j <= i; j++, num++) {
+                                System.err.print(" " + (num * num * num));
+                        }
+                        System.out.println();
+                }
+        }
 }
