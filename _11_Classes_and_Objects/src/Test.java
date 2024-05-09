1f1a0573a89df3f81d95bcb8141f491911c435ef
Aditya Ghosh <ghoshadityaxd@gmail.com>
2024-05-09 22:20:58 +0530

    test.java tested

--- _11_Classes_and_Objects/src/Test.java
+++ _11_Classes_and_Objects/src/Test.java
@@ -1,7 +1,37 @@
 package _11_Classes_and_Objects.src;
 
+class me {
+    // data or instances
+    String name;
+    int age;
+    String dob;
+    String bloodgrp;
+    String hobby;
+    String nickname;
+    String favanime;
+
+}
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        me obj = new me();
+        obj.name = "Aditya Ghosh";
+        obj.age = 19;
+        obj.bloodgrp = "A+";
+        obj.dob = "19 april 2003";
+        obj.hobby = "likes playing guitar";
+        obj.favanime = "Naruto";
+        obj.nickname = "andy";
+
+        // printing essentials :
+
+        System.err.println(obj.name);
+        System.err.println(obj.nickname);
+        System.err.println(obj.dob);
+        System.err.println(obj.age);
+        System.err.println(obj.bloodgrp);
+        System.err.println(obj.hobby);
+        System.err.println(obj.favanime);
+
     }
 }
